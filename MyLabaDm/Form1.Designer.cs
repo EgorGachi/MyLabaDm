670cf5c8d99a496d65492883cda41e7cb95e4cfc
egorm <egorm@DESKTOP-B7DPUHA>
2021-11-20 19:16:13 +0300

    Добавьте файлы проекта.

---
+++ MyLabaDm/Form1.Designer.cs
@@ -0,0 +1,116 @@
+﻿
+namespace MyLabaDm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.VertexDraw = new System.Windows.Forms.Button();
+            this.EdgeDraw = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(171, 21);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(684, 435);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.PictureBox1_MouseClick);
+            // 
+            // VertexDraw
+            // 
+            this.VertexDraw.Location = new System.Drawing.Point(879, 21);
+            this.VertexDraw.Name = "VertexDraw";
+            this.VertexDraw.Size = new System.Drawing.Size(43, 23);
+            this.VertexDraw.TabIndex = 1;
+            this.VertexDraw.Text = "V";
+            this.VertexDraw.UseVisualStyleBackColor = true;
+            this.VertexDraw.Click += new System.EventHandler(this.VertexButton_Click);
+            // 
+            // EdgeDraw
+            // 
+            this.EdgeDraw.Location = new System.Drawing.Point(928, 21);
+            this.EdgeDraw.Name = "EdgeDraw";
+            this.EdgeDraw.Size = new System.Drawing.Size(43, 23);
+            this.EdgeDraw.TabIndex = 2;
+            this.EdgeDraw.Text = "E";
+            this.EdgeDraw.UseVisualStyleBackColor = true;
+            this.EdgeDraw.Click += new System.EventHandler(this.EdgeButton_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(895, 362);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(181, 60);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Узнать сумму степеней всех вершин";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.SumEdge_Click);
+            // 
+            // label1
+            // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.label1.Location = new System.Drawing.Point(895, 294);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(178, 65);
+            this.label1.TabIndex = 4;
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label1.Click += new System.EventHandler(this.label1_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1278, 588);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.EdgeDraw);
+            this.Controls.Add(this.VertexDraw);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button VertexDraw;
+        private System.Windows.Forms.Button EdgeDraw;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ MyLabaDm/Form1.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MyLabaDm
+{
+    public partial class Form1 : Form 
+    {
+        Graph GRAPH;
+        List<Vertex> V;
+        List<Edge> E;
+        public int sumEdge = 0;
+        public int a = 0;
+
+        public int chosen1, chosen2;
+
+        public Form1()
+        {
+            InitializeComponent();
+            GRAPH = new Graph(pictureBox1.Width, pictureBox1.Height);
+            V = new List<Vertex>();
+            E = new List<Edge>();
+            pictureBox1.Image = GRAPH.GetBitmap();
+           
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+
+        private void VertexButton_Click(object sender, EventArgs e)
+        {
+            VertexDraw.Enabled = false;
+            EdgeDraw.Enabled = true;
+            GRAPH.Picturebox1Clear();
+            GRAPH.DRAWGRAF(V, E);
+            pictureBox1.Image = GRAPH.GetBitmap();
+            
+        }
+
+        private void EdgeButton_Click(object sender, EventArgs e)
+        {
+            EdgeDraw.Enabled = false;
+            VertexDraw.Enabled = true;
+            GRAPH.Picturebox1Clear();
+            GRAPH.DRAWGRAF(V, E);
+            pictureBox1.Image = GRAPH.GetBitmap();
+            chosen1 = -1;
+            chosen2 = -1;
+
+        }
+
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void CreateGraph_Click(object sender, EventArgs e)
+        {
+            //List<Vertex> vershini = new List<Vertex>();
+            //List<Edge> rebra = new List<Edge>();
+            //button1.Enabled = false;
+            //VertexDraw.Enabled = true;
+            //EdgeDraw.Enabled = true;
+            //GRAPH.Picturebox1Clear();
+            //GRAPH.DRAWGRAF(vershini, rebra);
+            //pictureBox1.Image = GRAPH.GetBitmap();
+
+        }
+
+        private void SumEdge_Click(object sender, EventArgs e)
+        {
+            int b = 0;
+            for (int i = 0; i < sumEdge; i++)
+            {
+                b = b + 1;
+                a = a + 1;
+
+            }
+            
+            label1.Text ="Сумма всех степеней вершин графа " +b.ToString();
+        }
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Sviaznii_Click(object sender, EventArgs e)
+        {
+            
+
+        }
+
+        private void PictureBox1_MouseClick(object sender, MouseEventArgs e)
+        {
+          
+            //рисую вершину
+            if (VertexDraw.Enabled==false)
+            {
+                V.Add(new Vertex(e.X,e.Y));
+                GRAPH.DrawVertex(e.X, e.Y, V.Count.ToString());
+                pictureBox1.Image = GRAPH.GetBitmap();
+                
+            }
+
+            //рисую ребро
+            if (EdgeDraw.Enabled==false)
+            {
+                if (e.Button==MouseButtons.Left)
+                {
+                    for (int i = 0; i < V.Count; i++)
+                    {
+                        if (Math.Pow(V[i].x - e.X, 2) + Math.Pow(V[i].y - e.Y, 2) <= GRAPH.R * GRAPH.R)
+                        {
+                            if (chosen1 == -1)
+                            {
+                                GRAPH.DrawSelectedVertex(V[i].x, V[i].y);
+                                chosen1 = i;
+                                pictureBox1.Image = GRAPH.GetBitmap();
+                                sumEdge += 1;
+                                break;
+                            }
+                            
+                            if (chosen2 == -1)
+                            {
+                                GRAPH.DrawSelectedVertex(V[i].x, V[i].y);
+                                chosen2 = i;
+                                E.Add(new Edge(chosen1, chosen2));
+                                GRAPH.DrawEdge(V[chosen1], V[chosen2], E[E.Count - 1]);
+                                chosen1 = -1;
+                                chosen2 = -1;
+                                pictureBox1.Image = GRAPH.GetBitmap();
+                                sumEdge += 1;
+                                break;
+                            }
+                           
+                        }
+                        
+                    }
+                }
+                //if (e.Button==MouseButtons.Right)
+                //{
+                //    if ((chosen1!=-1)&& Math.Pow(V[chosen1].x-e.X,2)+ Math.Pow(V[chosen1].y-e.Y,2)<=GRAPH.R*GRAPH.R)
+                //    {
+                //        GRAPH.DrawVertex(V[chosen1].x, V[chosen1].y, (chosen1 + 1).ToString());
+                //        chosen1 = -1;
+                //        pictureBox1.Image = GRAPH.GetBitmap();
+
+                //    }
+                //}
+            }
+
+
+
+        }
+    }
+}
